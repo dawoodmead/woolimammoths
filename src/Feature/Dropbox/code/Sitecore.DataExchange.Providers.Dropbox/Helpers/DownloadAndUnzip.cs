0766bb3381f231d6bba8feda984b73cbe9fc943e
Naveed Ahmad <naveed_ahmad@epam.com>
2017-03-04 18:26:20 -0500

    added comments

--- src/Feature/Dropbox/code/Sitecore.DataExchange.Providers.Dropbox/Helpers/DownloadAndUnzip.cs
+++ src/Feature/Dropbox/code/Sitecore.DataExchange.Providers.Dropbox/Helpers/DownloadAndUnzip.cs
@@ -15,19 +15,30 @@ namespace Sitecore.DataExchange.Providers.Dropbox.Helpers
         {
             try
             {
+                //download location for the dropbox URL
                 var downloadLocation = "\\temp\\dropbox\\";
+                
+                //extracting zip file location for the dropbox
                 var uploadLocation = "\\upload\\dropboxextract\\";
+                
+                //unique date time format
                 var datePart = DateTime.Now.ToString("yyyyMMMMddhhmmss");
+                
+                //full zip path
                 var zipPath = AssemblyDirectory + downloadLocation + datePart + ".zip";
+                
+                //full extraction path
                 var extractPath = AssemblyDirectory + uploadLocation + datePart;
-
+                
+                //fix url link for any dl=0
                 var url = FixLink(dropboxurl);
+                
+                //create the download location
                 if (!Directory.Exists(AssemblyDirectory + downloadLocation))
                 {
                     Directory.CreateDirectory(AssemblyDirectory + downloadLocation);
                 }
-
-
+                //if there is no extension in the url, download it as a  zip
                 if (!HasUrlExtensions(dropboxurl))
                 {
                     var client = new WebClient();
@@ -35,6 +46,7 @@ namespace Sitecore.DataExchange.Providers.Dropbox.Helpers
                     client.Dispose();
                     UnzipFiles(zipPath, extractPath);
                 }
+                //otherwise download it with the extension
                 else
                 {
                     var client = new WebClient();
@@ -46,7 +58,7 @@ namespace Sitecore.DataExchange.Providers.Dropbox.Helpers
             }
             catch (Exception ex)
             {
-
+                Sitecore.Diagnostics.Log.Error(ex.Message,ex,typeof(DownloadAndUnzipHelper));
 
             }
             
@@ -60,31 +72,37 @@ namespace Sitecore.DataExchange.Providers.Dropbox.Helpers
 
         private static void UnzipFiles(string zipPath, string extractPath)
         {
-
-
-            if (!Directory.Exists(extractPath))
+            try
             {
-                Directory.CreateDirectory(extractPath);
-            }
-            using (var archive = ZipFile.OpenRead(zipPath))
-            {
-                foreach (var entry in archive.Entries)
+                if (!Directory.Exists(extractPath))
                 {
-                    if (entry.Length > 0)
+                    Directory.CreateDirectory(extractPath);
+                }
+                using (var archive = ZipFile.OpenRead(zipPath))
+                {
+                    foreach (var entry in archive.Entries)
                     {
-                        if (entry.FullName.IndexOf('/') > 0)
+                        if (entry.Length > 0)
                         {
-                            CreateDirectoryRecursively(extractPath, entry.FullName);
-                        }
-
-                        if (!System.IO.File.Exists(extractPath + "\\" + entry.FullName.Replace("/", "\\")))
-                        {
-                            entry.ExtractToFile(extractPath + "\\" + entry.FullName.Replace("/", "\\"));
+                            //create directories recursively
+                            if (entry.FullName.IndexOf('/') > 0)
+                            {
+                                CreateDirectoryRecursively(extractPath, entry.FullName);
+                            }
+                            //extract file
+                            if (!System.IO.File.Exists(extractPath + "\\" + entry.FullName.Replace("/", "\\")))
+                            {
+                                entry.ExtractToFile(extractPath + "\\" + entry.FullName.Replace("/", "\\"));
+                            }
                         }
                     }
                 }
             }
-            
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error(ex.Message, ex, typeof(DownloadAndUnzipHelper));
+            }
+
         }
 
         private static void CreateDirectoryRecursively(string extractPath, string path)
@@ -95,36 +113,33 @@ namespace Sitecore.DataExchange.Providers.Dropbox.Helpers
             {
                 if (pathParts[i].Contains("."))
                     continue;
+
                 if (i > 0)
                     pathParts[i] = Path.Combine(pathParts[i - 1], pathParts[i]);
 
                 if (!Directory.Exists(extractPath + "\\" + pathParts[i]))
+                {
                     Directory.CreateDirectory(extractPath + "\\" + pathParts[i]);
+                }
             }
         }
-
         public static string AssemblyDirectory
         {
             get
             {
-                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                UriBuilder uri = new UriBuilder(codeBase);
-                string path = Uri.UnescapeDataString(uri.Path);
-                return Path.GetDirectoryName(path).Replace("\\bin",string.Empty);
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path)?.Replace("\\bin",string.Empty);
             }
         }
-
         private static bool HasUrlExtensions(string url)
         {
             Uri uri = new Uri(url);
             return Path.HasExtension(uri.AbsoluteUri);
-
-            
         }
-
         private static string GetUrlExtension(string url)
         {
-
             return Path.GetExtension(url);
         }
     }
