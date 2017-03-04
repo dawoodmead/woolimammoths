0b9f2d43fabfb29bb2e59105032087be676fa9a1
Konstantin Cherkasov <Konstantin_Cherkasov@epam.com>
2017-03-03 16:57:58 -0800

    Empty solution

---
+++ src/code/WooliMammoths.Module/Services/IocConfigurationService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace WooliMammoths.Module.Services
+{
+    public class IocConfigurationService : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMvcControllersInCurrentAssembly();
+            // serviceCollection.AddTransient()
+        }
+    }
+}
