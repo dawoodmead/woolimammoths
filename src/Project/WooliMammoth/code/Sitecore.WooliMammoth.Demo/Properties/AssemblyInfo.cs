7f93b5f6ac09277967ab6bdbcec0cb3927a1b2e6
Konstantin Cherkasov <Konstantin_Cherkasov@epam.com>
2017-03-04 09:54:57 -0800

    Reconstruction

--- src/Project/WooliMammoth/code/Sitecore.WooliMammoth.Demo/Properties/AssemblyInfo.cs
+++ src/Project/WooliMammoth/code/Sitecore.WooliMammoth.Demo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DropboxHelloWorld")]
+[assembly: AssemblyTitle("Sitecore.WooliMammoth.Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EPAM Systems")]
-[assembly: AssemblyProduct("DropboxHelloWorld")]
-[assembly: AssemblyCopyright("Copyright © EPAM Systems 2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sitecore.WooliMammoth.Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a8d1af8b-0f15-410a-81bd-4c26143ad3b8")]
+[assembly: Guid("af93ceeb-a0d9-4237-ba52-c8ba22893d24")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
